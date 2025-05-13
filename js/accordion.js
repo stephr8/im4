2fbb242a47f3aadc481a2e183dbb8cc75072b374
buryar <yara.burkhalter@stud.fhgr.ch>
2025-05-13 23:11:31 +0200

    erster akkordeon versuch

--- js/accordion.js
+++ js/accordion.js
@@ -1,7 +1,8 @@
-document.querySelectorAll('.accordion-header').forEach(button => {
+/* document.querySelectorAll('.accordion-header').forEach(button => {
   button.addEventListener('click', () => {
     const content = button.nextElementSibling;
 
     content.style.display = content.style.display === 'block' ? 'none' : 'block';
   });
-});
+}); */
+
