f12e8f649cfcf29b8a5a279971c749a83bd60619
buryar <yara.burkhalter@stud.fhgr.ch>
2025-05-15 10:44:04 +0200

    accordion kleiner machen

--- js/accordion.js
+++ js/accordion.js
@@ -2,7 +2,10 @@
       const isChecked = checkbox.src.includes('uncheckedmark.svg');
       checkbox.src = isChecked ? 'images/checkedmark.svg' : 'images/uncheckedmark.svg';
     }
- 
+ function toggleAccordion(header) {
+      const accordion = header.closest(".accordion");
+      accordion.classList.toggle("open");
+  }
     function toggleAccordion(header) {
     const accordion = header.parentElement;
     const body = accordion.querySelector('.accordion-body');
