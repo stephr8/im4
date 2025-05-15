d0dc39bc6f23161ab97563f36f5bd2ae6390b0bf
stephr8 <stephr.zh@gmail.com>
2025-05-15 13:57:38 +0200

    read profile works

--- api/profile/readProfile.php
+++ api/profile/readProfile.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+header('Content-Type: application/json');
 
 if (!isset($_SESSION['user_id'])) {
     http_response_code(401);
@@ -14,22 +15,21 @@ require_once '../../system/config.php';
 // Logged-in user ID
 $loggedInUserId = $_SESSION['user_id'];
 
+
 // Get the logged-in user's data
-$stmt = $pdo->prepare("SELECT user_id, first_name, last_name, email, username, street, zip, town, birthdate, nationality, ahv, trust_person FROM user_profiles WHERE user_id = :user_id");
+$stmt = $pdo->prepare("SELECT up.user_id, up.first_name, up.last_name, u.email, up.username, up.street, up.zip, up.town, up.birthdate, up.nationality, up.ahv, up.trust_person FROM user_profiles as up, users as u WHERE  u.id = up.user_id and up.user_id = :user_id");
 $stmt->bindParam(':user_id', $loggedInUserId, PDO::PARAM_INT);
 $stmt->execute();
 
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
 if ($user) {
-    header('Content-Type: application/json');
     echo json_encode([
         "status" => "success",
         "user" => $user
     ]);
 } else {
     http_response_code(404);
-    header('Content-Type: application/json');
     echo json_encode(["error" => "User not found"]);
 }
 ?>
--- js/profile.js
+++ js/profile.js
@@ -1,67 +1,102 @@
-    // _______________________________________________________________
-    // Loading the profile data from the API
-    // _______________________________________________________________
+// _______________________________________________________________
+// Loading the profile data from the API
+// _______________________________________________________________
 
-    async function loadData() {
-        const url = '/api/profile/readProfile.php'; // mit korrekter API-URL ersetzen
-        try {
-            const response = await fetch(url);
-            return await response.json();
-        } catch (error) {
-            console.error(error);
-            return false;
-        }
-    }
-    const data = await loadData();
-    console.log(data);// gibt die Daten der API oder false in der Konsole aus
+console.log('Loading profile data...');
+async function loadData() {
+  const url = '/api/profile/readProfile.php'; // mit korrekter API-URL ersetzen
+  try {
+    const response = await fetch(url);
+    console.log(response);
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+const data = await loadData();
+console.log(data);// gibt die Daten der API oder false in der Konsole aus
 
-    const domfirstName = document.querySelector('#firstName');
-    const domlastName = document.querySelector('#lastName');
+const domfirstName = document.querySelector('#first_name');
+const domlastName = document.querySelector('#last_name');
+const domusername = document.querySelector('#username');
+const domstreet = document.querySelector('#street');
+const domzip = document.querySelector('#zip');
+const domtown = document.querySelector('#town');
+const dombirthdate = document.querySelector('#birthdate');
+const domnationality = document.querySelector('#nationality');
+const domtrust_person = document.querySelector('#trust_person');
+const saveButton = document.querySelector('#save_user_information');
+const domemail = document.querySelector('#email');
 
-    domfirstName.innerHTML = data.user.first_name;
-    domlastName.innerHTML = data.user.last_name;
 
-    // _______________________________________________________________
-    // Adding Vorname und Nachname to the Database
-    // _______________________________________________________________
+domfirstName.value = data.user.first_name;
+domlastName.value = data.user.last_name;
+domusername.value = data.user.username;
+domstreet.value = data.user.street;
+domzip.value = data.user.zip;
+domtown.value = data.user.town;
+dombirthdate.value = data.user.birthdate;
+domnationality.value = data.user.nationality;
+domtrust_person.value = data.user.trust_person;
+domemail.value = data.user.email;
+// _______________________________________________________________
+// Adding Vorname und Nachname to the Database
+// _______________________________________________________________
 
 
-    const inputFirstName = document.querySelector('#inputFirstName');
-    const inputLastName = document.querySelector('#inputLastName');
-    const inputUsername = document.querySelector('#inputUsername');
-    const inputStreet = document.querySelector('#inputStreet');
-    const inputZip = document.querySelector('#inputZip');
-    const inputTown = document.querySelector('#inputTown');
-    const inputBirthdate = document.querySelector('#inputBirthdate');
-    const inputNationality = document.querySelector('#inputNationality');
-    const inputTrust_person = document.querySelector('#inputTrust_person');
-    const saveButton = document.querySelector('#Btn_saveInfo');
+// const inputFirstName = document.querySelector('#inputFirstName');
+// const inputLastName = document.querySelector('#inputLastName');
+// const inputUsername = document.querySelector('#inputUsername');
+// const inputStreet = document.querySelector('#inputStreet');
+// const inputZip = document.querySelector('#inputZip');
+// const inputTown = document.querySelector('#inputTown');
+// const inputBirthdate = document.querySelector('#inputBirthdate');
+// const inputNationality = document.querySelector('#inputNationality');
+// const inputTrust_person = document.querySelector('#inputTrust_person');
 
-    saveButton.addEventListener('click', async () => {
-        let firstName = inputFirstName.value;
-        let lastName = inputLastName.value;
-        const url = '/api/profile/createProfile.php'; // mit korrekter API-URL ersetzen
-        const data = {
-            first_name: firstName,
-            last_name: lastName
-        };
-        const dataAdded = await addData(url, data);
-        console.log(dataAdded);
+
+saveButton.addEventListener('click', async () => {
+  let firstName = inputFirstName.value;
+  let lastName = inputLastName.value;
+  let username = inputUsername.value;
+  let street = inputStreet.value;
+  let zip = inputZip.value;
+  let town = inputTown.value;
+  let birthdate = inputBirthdate.value;
+  let nationality = inputNationality.value;
+  let trust_person = inputTrust_person.value;
+  let email = domemail.value;
+  const url = '/api/profile/createProfile.php'; // mit korrekter API-URL ersetzen
+  const data = {
+    first_name: firstName,
+    last_name: lastName,
+    username: username,
+    street: street,
+    zip: zip,
+    town: town,
+    birthdate: birthdate,
+    nationality: nationality,
+    trust_person: trust_person,
+    email: email
+  };
+  const dataAdded = await addData(url, data);
+  console.log(dataAdded);
+});
+
+//I want to post the data to the API and then reload the page
+async function addData(url, data) {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
     });
-
-    //I want to post the data to the API and then reload the page
-    async function addData(url, data) {
-        try {
-            const response = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            });
-            return await response.json();
-        } catch (error) {
-            console.error(error);
-            return false;
-        }
-    }
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
